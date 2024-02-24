fdd3888b23d8c28711c412afc2d615270cfc94aa
David Sapiro <dsap48@gmail.com>
2024-02-23 16:32:19 -0800

    added display board and submit word functions

--- static/boggle.js
+++ static/boggle.js
@@ -26,9 +26,45 @@ async function start() {
 /** Display board */
 
 function displayBoard(board) {
-  // $table.empty();
+  $table.empty();
   // loop over board and create the DOM tr/td structure
+  for (const row of board) {
+    const $row = $("<tr>");
+    for (const cell of row) {
+      const $cell = $("<td>").text(cell);
+      $row.append($cell);
+    }
+    $table.append($row);
+  }
 }
 
+/** Grabs entered word from input field */
 
+function getWord() {
+  return $wordInput.val();
+}
+
+/** Submits entered word to the server and receives response. */
+
+async function submitWord(word) {
+  const response = await fetch('/api/score-word', {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      "gameId": gameId,
+      "word": word
+    })
+  });
+
+  const result = await response.json();
+  return result;
+}
+
+function handleSubmit(evt) {
+  const word = getWord();
+  const word_response = submitWord(word);
+
+}
+
+$form.on("submit", handleClick);
 start();
